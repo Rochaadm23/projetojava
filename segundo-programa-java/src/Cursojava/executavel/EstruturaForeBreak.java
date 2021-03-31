190e44c9fffe49239889d1a9b7533ed23742bfe4
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-03-31 19:48:30 -0300

    Adicionando Estrutura de repetição com for e break.

---
+++ segundo-programa-java/src/Cursojava/executavel/EstruturaForeBreak.java
@@ -0,0 +1,16 @@
+package Cursojava.executavel;
+
+public class EstruturaForeBreak {
+
+	public static void main(String[] args) {
+		/*Estrutura for com BREAK*/
+		for(int numero = 0; numero <=10; numero++) {
+			if(numero == 2) {
+				System.out.println("Numero atual é: " + numero);
+				break;
+			}
+		}
+
+	}
+
+}
