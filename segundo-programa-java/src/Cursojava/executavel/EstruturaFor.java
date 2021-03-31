905636ced7086e3482446b8c1bb24972b63795d9
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-03-31 19:05:45 -0300

    Adicionando a Estrutura de repetição FOR.

---
+++ segundo-programa-java/src/Cursojava/executavel/EstruturaFor.java
@@ -0,0 +1,13 @@
+package Cursojava.executavel;
+
+public class EstruturaFor {
+
+	public static void main(String[] args) {
+		/*Estrutura de repetição FOR*/
+		for(int numero = 10; numero >= 0; numero-- ) {
+			System.out.println("O numero atual é " + numero);
+		}
+	}
+	
+	
+}
