d80a37dbf3de2ef4dbb7e31439cf99288e85a7fe
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-04 20:25:09 -0300

    Criado a classe pai Pessoa e as filhas Aluno, diretor e Secretario utilizando a herança da classe Pessoa.

--- segundo-programa-java/src/cursojava/classes/Aluno.java
+++ segundo-programa-java/src/cursojava/classes/Aluno.java
@@ -5,15 +5,9 @@ import java.util.List;
 
 import cursojava.constantes.StatusAluno;
 
-public class Aluno {
+public class Aluno extends Pessoa {
 	/* Atributos da classe aluno */
-	private String nome;
-	private int idade;
-	private String dataNascimento;
-	private String registroGeral;
-	private String numeroCpf;
-	private String nomeMae;
-	private String nomePai;
+
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
