903c8875310ca075b5b6a37064328b32bcdbe894
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-10 23:34:50 -0300

    Recuperando valores no Array

--- segundo-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ segundo-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -2,18 +2,20 @@ package cursojava.executavel;
 
 public class ArrayVetor {
 	public static void main(String[] args) {
-		
-		/*Array pode ser de todos os tipos de dados e objetos*/
-		/*Array sempre deve ter a quantidade de posições definidas*/
-		double notas[] = new double[4];  
+
+		/* Array pode ser de todos os tipos de dados e objetos */
+		/* Array sempre deve ter a quantidade de posições definidas */
+		double notas[] = new double[4];
 		notas[0] = 9.9;
 		notas[1] = 8.7;
 		notas[2] = 9.7;
 		notas[3] = 9.9;
-		
-		
-		System.out.println(notas);
-		
-		
+
+		for (int i = 0; i < notas.length; i++) {
+
+			System.out.println("Nota " + (i + 1) + " é = " + notas[i]);
+
+		}
+
 	}
 }
