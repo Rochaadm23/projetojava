0bf52ba5af005f19edc8f59261e6b1798db4cfc2
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-17 22:02:32 -0300

    AULA - Criando uma Matriz multidimensional

---
+++ segundo-programa-java/src/cursojava/executavel/Matriz.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+public class Matriz {
+	public static void main(String[] args) {
+		
+		/* MATRIZ COM 1 POSIÇÃO + 3 ELEMENTOS*/
+		int notas[][] = new int[2][3];
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 70;
+		
+		notas[1][0] = 40;
+		notas[1][1] = 50;
+		notas[1][2] = 70;
+		
+		
+		System.out.println(notas);
+		
+	}
+}
