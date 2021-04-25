1051375075b3bc02fc65f8b52d7f7ff06613a3ad
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-25 12:19:53 -0300

    Lendo arquivo TXT e criando lista de objetos

--- segundo-programa-java/src/cursojava/arquivos/LerArquivoTXT.java
+++ segundo-programa-java/src/cursojava/arquivos/LerArquivoTXT.java
@@ -3,6 +3,8 @@ package cursojava.arquivos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTXT {
@@ -12,15 +14,31 @@ public class LerArquivoTXT {
 		FileInputStream entradaArquivo = new FileInputStream(
 				new File("/home/rocha/git/repository/segundo-programa-java/src/cursojava/arquivos/arquivo.txt"));
 
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 
 		while (lerArquivo.hasNext()) {
 			String linha = lerArquivo.nextLine();
+
 			if (linha != null && !linha.isEmpty()) {
-				System.out.println(linha);
+				String[] dados = linha.split("\\;");
+
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setIdade(Integer.parseInt(dados[1]));
+				pessoa.setEmail(dados[2]);
+
+				pessoas.add(pessoa);
 
 			}
 		}
 
+		for (Pessoa pessoa : pessoas) {/*
+										 * Poderia gravar no banco de dados, enviar e-mail, gerar boleto, fazer qualquer
+										 * coisa
+										 */
+			System.out.println(pessoa);
+		}
 	}
 }
